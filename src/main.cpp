89153186c73827ff9779732e3c8a581c611b7d89
lbehrler <38298678+lbehrler@users.noreply.github.com>
2023-01-08 16:29:22 -0600

    Code Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
   of ADC values and and prints the result to the display and debug to Serial Monitor.
 
   Based on prior EANx scripts: 
-  https://github.com/ppppaoppp/DIY-Nitrox-Analyzer-04_12_2019.git
+  https://github.com/ppppaoppp/DIY-Nitrox-Analyzer-04_12_2019.gitf
   https://github.com/ejlabs/arduino-nitrox-analyzer.git
 
 *****************************************************************************/
@@ -32,15 +32,6 @@
   #define debug(x)
   #define debugln(x)
 #endif
-
-//Functions
-void o2calibration();
-float initADC();
-void printLayout();
-void testfillcircles(uint8_t radius, uint16_t color);
-void testdrawcircles(uint8_t radius, uint16_t color);
-void safetyrule();
-float batStat();
 
 // display definitions
 #define TFT_WIDTH 240   // OLED display width, in pixels
@@ -70,6 +61,134 @@ float multiplier = 0;
 int msgid = 0;
 
 const int buttonPin = BUTTON_PIN;  // push button
+
+//Functions
+float batStat();
+
+float initADC() {
+  // init ADC and Set gain
+
+  // The ADC input range (or gain) can be changed via the following
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  ads.setGain(GAIN_TWO);  // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  /* Be sure to update this value based on the IC and the gain settings! */
+  //float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
+  float multiplier = 0.0625; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+
+  // Check that the ADC is operational
+  if (!ads.begin()) {
+    debugln("Failed to initialize ADS.");
+    tft.fillScreen(TFT_YELLOW);
+    tft.setTextColor(TFT_RED);
+    tft.setTextSize(1 * ResFact);
+    tft.drawCentreString("Error", TFT_WIDTH * .5, TFT_HEIGHT * 0, 4);
+    tft.drawCentreString("No ADC", TFT_WIDTH * .5, TFT_HEIGHT * 0.3, 4);
+    delay(5000);
+    while (1)
+      ;
+  }
+  return (multiplier);
+}
+
+void o2calibration() {
+  //display "Calibrating"
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(1 * ResFact);
+  tft.drawCentreString("+++++++++++++", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+  tft.drawCentreString("Calibrating", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
+  tft.drawCentreString("O2 Sensor", TFT_WIDTH * .5, TFT_HEIGHT * .60, 2);
+  tft.drawCentreString("+++++++++++++", TFT_WIDTH * .5, TFT_HEIGHT * .80, 2);
+  debugln("Calibration Screen Text");
+
+  initADC();
+
+  debugln("Post ADS check statement");
+  // get running average value from ADC input Pin
+  RA.clear();
+  for (int x = 0; x <= (RA_SIZE * 3); x++) {
+    int sensorValue = 0;
+    sensorValue = ads.readADC_Differential_0_1();
+    RA.addValue(sensorValue);
+    delay(16);
+    // debug("calibrating ");
+    // debugln(sensorValue);  //raw sensor serial print for debugging
+  }
+  debug("average calibration read");
+  debugln(RA.getAverage());  // average cal factor serial print for debugging
+
+  tft.fillScreen(TFT_BLACK);
+  calFactor = (1 / RA.getAverage() * 20.900);  // Auto Calibrate to 20.9%
+
+  debug("calibrated ");
+  debugln(calFactor);  // average cal factor serial print for debugging
+}
+
+// Draw Layout -- Adjust this layouts to suit you LCD
+void printLayout() {
+  tft.setTextSize(1 * ResFact);
+  tft.setTextColor(TFT_MAGENTA, TFT_BLACK);
+  tft.drawCentreString("O2 %", TFT_WIDTH * .50, TFT_HEIGHT * .01, 4);
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.drawString("Info", TFT_WIDTH * .10, TFT_HEIGHT * .6, 2);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.drawString("MOD", TFT_WIDTH * .60, TFT_HEIGHT * .6, 2);
+}
+
+
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x = radius; x < tft.width(); x += radius * 2) {
+    for (int16_t y = radius; y < tft.height(); y += radius * 2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x = 0; x < tft.width() + radius; x += radius * 2) {
+    for (int16_t y = 0; y < tft.height() + radius; y += radius * 2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void safetyrule() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_YELLOW);
+  tft.setTextSize(1 * ResFact);
+  randomSeed(millis());
+  int randNumber = random(5);
+  if (randNumber == 0) {
+    tft.drawCentreString("Seek proper", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+    tft.drawCentreString("training", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
+  } else if (randNumber == 1) {
+    tft.drawCentreString("Maintain a", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+    tft.drawCentreString("continious", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
+    tft.drawCentreString("guideline to", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
+    tft.drawCentreString("the surface", TFT_WIDTH * .5, TFT_HEIGHT * .40, 2);
+  } else if (randNumber == 2) {
+    tft.drawCentreString("Stay within", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+    tft.drawCentreString("your depth", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
+    tft.drawCentreString("limitations", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
+  } else if (randNumber == 3) {
+    tft.drawCentreString("Proper gas", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+    tft.drawCentreString("management", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
+  } else {
+    tft.drawCentreString("Use appropriate", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
+    tft.drawCentreString("properly maintaned", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
+    tft.drawCentreString("equipment", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
+  }
+  delay(3000);
+  tft.fillScreen(TFT_BLACK);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -195,127 +314,4 @@ void loop() {
     tft.drawString(String(modm + " m  "), TFT_WIDTH * .6, TFT_HEIGHT * .83, 2);
 
   }
-}
-
-void o2calibration() {
-  //display "Calibrating"
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(1 * ResFact);
-  tft.drawCentreString("+++++++++++++", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-  tft.drawCentreString("Calibrating", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
-  tft.drawCentreString("O2 Sensor", TFT_WIDTH * .5, TFT_HEIGHT * .60, 2);
-  tft.drawCentreString("+++++++++++++", TFT_WIDTH * .5, TFT_HEIGHT * .80, 2);
-  debugln("Calibration Screen Text");
-
-  initADC();
-
-  debugln("Post ADS check statement");
-  // get running average value from ADC input Pin
-  RA.clear();
-  for (int x = 0; x <= (RA_SIZE * 3); x++) {
-    int sensorValue = 0;
-    sensorValue = ads.readADC_Differential_0_1();
-    RA.addValue(sensorValue);
-    delay(16);
-    // debug("calibrating ");
-    // debugln(sensorValue);  //raw sensor serial print for debugging
-  }
-  debug("average calibration read");
-  debugln(RA.getAverage());  // average cal factor serial print for debugging
-
-  tft.fillScreen(TFT_BLACK);
-  calFactor = (1 / RA.getAverage() * 20.900);  // Auto Calibrate to 20.9%
-
-  debug("calibrated ");
-  debugln(calFactor);  // average cal factor serial print for debugging
-}
-
-// Draw Layout -- Adjust this layouts to suit you LCD
-void printLayout() {
-  tft.setTextSize(1 * ResFact);
-  tft.setTextColor(TFT_MAGENTA, TFT_BLACK);
-  tft.drawCentreString("O2 %", TFT_WIDTH * .50, TFT_HEIGHT * .01, 4);
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.drawString("Info", TFT_WIDTH * .10, TFT_HEIGHT * .6, 2);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.drawString("MOD", TFT_WIDTH * .60, TFT_HEIGHT * .6, 2);
-}
-
-float initADC() {
-  // init ADC and Set gain
-
-  // The ADC input range (or gain) can be changed via the following
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  ads.setGain(GAIN_TWO);  // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
-  /* Be sure to update this value based on the IC and the gain settings! */
-  //float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
-  float multiplier = 0.0625; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
-
-  // Check that the ADC is operational
-  if (!ads.begin()) {
-    debugln("Failed to initialize ADS.");
-    tft.fillScreen(TFT_YELLOW);
-    tft.setTextColor(TFT_RED);
-    tft.setTextSize(1 * ResFact);
-    tft.drawCentreString("Error", TFT_WIDTH * .5, TFT_HEIGHT * 0, 4);
-    tft.drawCentreString("No ADC", TFT_WIDTH * .5, TFT_HEIGHT * 0.3, 4);
-    delay(5000);
-    while (1)
-      ;
-  }
-  return (multiplier);
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x = radius; x < tft.width(); x += radius * 2) {
-    for (int16_t y = radius; y < tft.height(); y += radius * 2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x = 0; x < tft.width() + radius; x += radius * 2) {
-    for (int16_t y = 0; y < tft.height() + radius; y += radius * 2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
-
-void safetyrule() {
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_YELLOW);
-  tft.setTextSize(1 * ResFact);
-  randomSeed(millis());
-  int randNumber = random(5);
-  if (randNumber == 0) {
-    tft.drawCentreString("Seek proper", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-    tft.drawCentreString("training", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
-  } else if (randNumber == 1) {
-    tft.drawCentreString("Maintain a", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-    tft.drawCentreString("continious", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
-    tft.drawCentreString("guideline to", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
-    tft.drawCentreString("the surface", TFT_WIDTH * .5, TFT_HEIGHT * .40, 2);
-  } else if (randNumber == 2) {
-    tft.drawCentreString("Stay within", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-    tft.drawCentreString("your depth", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
-    tft.drawCentreString("limitations", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
-  } else if (randNumber == 3) {
-    tft.drawCentreString("Proper gas", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-    tft.drawCentreString("management", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
-  } else {
-    tft.drawCentreString("Use appropriate", TFT_WIDTH * .5, TFT_HEIGHT * .10, 2);
-    tft.drawCentreString("properly maintaned", TFT_WIDTH * .5, TFT_HEIGHT * .20, 2);
-    tft.drawCentreString("equipment", TFT_WIDTH * .5, TFT_HEIGHT * .30, 2);
-  }
-  delay(3000);
-  tft.fillScreen(TFT_BLACK);
 }
